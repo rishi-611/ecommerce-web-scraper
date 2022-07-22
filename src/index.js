26f53b5c9ec2b851ec37b20ee64b9a9fed537939
rishi-18454 <rishikesh@hybr1d.io>
2022-07-22 23:46:35 +0530

    added code for laptop listing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import Puppeteer from "puppeteer";
 import colors from "colors";
+// import devices from "./data/devices.json" assert { type: "json" };
+
+// const device = devices[0];
 
 const start = async () => {
   const browser = await Puppeteer.launch({
     headless: false, //set true when not testing
   });
-  const page = await browser.newPage();
-  await page.goto("https://www.reliancedigital.in");
+  try {
+    const page = await browser.newPage();
+    await page.goto("https://www.croma.com/");
 
-  await browser.close;
+    const laptopsListing = await page.$$eval(
+      ".swiper-slide .slide-img-wrap a",
+      (categories) => {
+        console.log(categories);
+        const final = categories.filter((category) => {
+          return (
+            category.querySelector("span > img")?.alt?.toLowerCase() ===
+            "laptops"
+          );
+        });
+
+        if (!final?.length)
+          throw new Error({
+            message: "type: laptops not found on home page",
+          });
+        return final[0].href;
+      }
+    );
+
+    page.goto(laptopsListing, {
+      waitUntil: "networkidle2",
+    });
+
+    // await browser.close();
+  } catch (error) {
+    // await browser.close();
+    throw new Error(error);
+  }
 };
 
 start()
