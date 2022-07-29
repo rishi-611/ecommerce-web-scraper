f3735cef33559da88fa9ed21ba1f97847600a0ea
rishi-18454 <rishikesh@hybr1d.io>
2022-07-29 18:36:34 +0530

    added scrape currys to main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import colors from "colors";
-import currency from "currency.js";
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -24,15 +23,15 @@ const init = async () => {
   updateDevices(devices, priceList);
   console.log("updated devices.csv".green);
 
-  // //scrape croma website for each device, and update device info
-  // let priceList = await scrapeCroma(devices);
-  // updateDevices(devices, priceList);
-  // console.log("updated devices.csv".green);
+  //scrape croma website for each device, and update device info
+  priceList = await scrapeCroma(devices);
+  updateDevices(devices, priceList);
+  console.log("updated devices.csv".green);
 
-  // //scrape johnlewis
-  // priceList = await scrapeJohnLewis(devices);
-  // updateDevices(devices, priceList);
-  // console.log("updated devices.csv".green);
+  //scrape johnlewis
+  priceList = await scrapeJohnLewis(devices);
+  updateDevices(devices, priceList);
+  console.log("updated devices.csv".green);
 
   //convert json to csv, and overwrite existing file
   const parser = new Parser();
